c298e34b21662b1ed7344edf45a80bc1e7ab160a
deltaDAO Dev <luca@external.delta-dao.com>
2024-08-24 17:17:04 +0200

    update onboarding module

--- src/components/@shared/Onboarding/StepHeader.tsx
+++ src/components/@shared/Onboarding/StepHeader.tsx
@@ -1,5 +1,8 @@
 import { ReactElement } from 'react'
 import styles from './StepHeader.module.css'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
 
 export default function StepHeader({
   title,
@@ -10,7 +13,9 @@ export default function StepHeader({
 }): ReactElement {
   return (
     <div className={styles.header}>
-      <h3 className={styles.title}>{title}</h3>
+      <div className={cx({ underlinedTitle: true })}>
+        <h3 className={styles.title}>{title}</h3>
+      </div>
       <h5 className={styles.subtitle}>{subtitle}</h5>
     </div>
   )
